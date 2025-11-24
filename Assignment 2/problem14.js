e59e0753ff1836d4b7bf95ad78a799be12a8fb55
nandhini673494 <nandhinid739@gmail.com>
2025-11-24 10:34:41 +0530

    Add files via upload

---
+++ Assignment 2/problem14.js
@@ -0,0 +1,3 @@
+let apps=["Instagram","Youtube","LinkedIn"];
+let [first,second]= apps;
+console.log(first,second);
---
+++ Assignment 2/problem3.js
@@ -0,0 +1,6 @@
+let name ="Nandhini";
+console.log('Hello, ${name}! Welcome to Javascript.');
+console.log( name.toUpperCase());
+let msg= 'Hello, ${name}! Welcome to Javascript';
+console.log(msg.length > 10);
+console.log(msg.includes("Javascript"));
